cec45d24d86fa9d1e74cfbdbf5aac3708fa3a507
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-08-03 10:14:27 -0600

    Add method to find all the passenger names for a fight

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,9 @@
 class Flight <ApplicationRecord
   has_many :passenger_flights
   has_many :passengers, through: :passenger_flights
+
+  def all_passenger_names
+    all = self.passengers.map { |passenger| passenger.name}
+    all.join(", ")
+  end
 end
