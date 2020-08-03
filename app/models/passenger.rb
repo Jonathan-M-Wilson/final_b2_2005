6b31899183fa1632ceb98e74fabe7981cc7e6470
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-08-03 11:51:52 -0600

    Add methods to find count of minors and adults

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,12 @@
 class Passenger <ApplicationRecord
   has_many :passenger_flights
   has_many :flights, through: :passenger_flights
+
+  def self.minors_on_flight
+    Passenger.where('age < ?', 18).count
+  end
+  
+  def self.adults_on_flight
+    Passenger.where('age >= ?', 18).count
+  end
 end
