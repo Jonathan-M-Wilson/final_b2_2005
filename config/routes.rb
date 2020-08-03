2400b6d2e2f77a2f43ce1ce949e754006cb9b324
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-08-03 11:42:45 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/flights/:id", to: 'flights#show'
+  get "/passengers/:id", to: "passengers#show"
+  post "passengers/:passenger_id/passenger_flights", to: "passenger_flights#create"
 end
